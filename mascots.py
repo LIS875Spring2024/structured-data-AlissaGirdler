04196fd091d263ea4a98980a6c52a07705bdc1d7
Alissa Girdler <alissa.girdler@gmail.com>
2024-02-27 20:34:15 -0600

    Lab exercises 1 to 7

--- mascots.py
+++ mascots.py
@@ -4,8 +4,14 @@ mascots = {
     "Michigan": "Wolverines",
     "Wisconsin": "Badgers",
     "Ohio State": "Buckeyes",
-    "Penn State": "Nittany Lions"
-    }
+    "Penn State": "Nittany Lions",
+    "Indiana": "Hoosiers"
+}
 
-print("People who go to Wisconsin are called", mascots["Wisconsin"])
+university = input("What university's mascot are you looking for? ")
+if university in mascots:
+    print("People who go to", university, "are called", mascots[university])
+else:
+    print("Mascot information not found for", university)
+
 
--- student.py
+++ student.py
@@ -1,9 +1,24 @@
 
-student = {
+first_student = {
     'name': "Aaron Enright",
     'major': "Library and Information Science",
-    'favorite_food': "pizza"
+    'favorite_food': "pizza",
+    'favorite_color' : "blue"
     }
 
-print("The student's name is", student['name'])
-print("The student's major is", student['major'])
+second_student = {
+    'name': "Alissa Girdler",
+    'major': "Information",
+    'favorite_food': "Sushi",
+    'favorite_color' : "Sage"
+    }
+
+print("The first student's name is", first_student['name'])
+print("The first student's major is", first_student['major'])
+print("The first student's major is", first_student['favorite_food'])
+print("The first student's major is", first_student['favorite_color'])
+print(" ")
+print("The second student's name is", second_student['name'])
+print("The second student's major is", second_student['major'])
+print("The second student's major is", second_student['favorite_food'])
+print("The second student's major is", second_student['favorite_color'])
